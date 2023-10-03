1e6ea0cc7330b25150fe2cbd539118a44e37edfc
BuckDaTruck <buckleywiley@gmail.com>
2023-10-03 09:12:32 -0400

    commit

---
+++ Unit_3/iteration through strings/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println(repeatSeparator("Word", "X", 3));
+
+    }
+public static String repeatSeparator(String word, String sep, int count) {
+    String result = "";
+    for (int i = 0; i < count; i++) {
+        result += word;
+        if (i < count - 1) {
+            result += sep;
+        }
+    }
+    return result;
+}
+}
---
+++ Unit_3/iteration through strings/instrucitons.md
@@ -0,0 +1,11 @@
+Iterating through Strings
+Name: _______________________________
+
+In addition to the String methods you are familiar with from your Java quick reference, you may find the following helpful:
+
+boolean startsWith(String chars)
+Returns true if this starts with chars
+boolean endsWith(String chars)
+Returns true if this ends with chars
+
+
