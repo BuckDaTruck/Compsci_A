f69a65cf62cd500b82da7aaac268d48813090b97
BuckDaTruck <buckleywiley@gmail.com>
2023-10-03 09:27:36 -0400

    work

--- Unit_3/iteration through strings/instrucitons.md
+++ Unit_3/iteration through strings/instrucitons.md
@@ -1,5 +1,5 @@
 Iterating through Strings
-Name: _______________________________
+Name: Buckley
 
 In addition to the String methods you are familiar with from your Java quick reference, you may find the following helpful:
 
@@ -9,3 +9,33 @@ boolean endsWith(String chars)
 Returns true if this ends with chars
 
 
+
+Given two Strings, word and a separator sep, return a big String made of count occurrences of the word, separated by the separator String.
+repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+repeatSeparator("This", "And", 2) → "ThisAndThis"
+repeatSeparator("This", "And", 1) → "This”
+
+Given a String, consider the prefix string made of the first N chars of the string. Does that prefix appear somewhere else in the String? Assume that the String is not empty and that N is in the range [1, str.length()).
+prefixAgain("abXYabc", 1) → true
+prefixAgain("abXYabc", 2) → true
+prefixAgain("abXYabc", 3) → false
+
+Write a function that returns true if the Strings “cat” and “dog” appear the same number of times in the given String.
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
+
+Write a function that, given two Strings, returns true if either of the Strings appear at the very end of the other String, ignoring differences in casing.
+endOther("Hiabc", "abc") → true
+endOther("AbC", "HiaBc") → true
+endOther("abc", "abXabc") → true
+
+Write a function that returns true if the given String contains a "bob" String, but where the middle 'o' char can be any char.
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+
+Given two Strings, a and b, write a function that returns a bigger String made of the first char of a, the first char of b, the second char of a, the second char of b, and so on. Any leftover chars go at the end of the result.
+mixString("abc", "xyz") → "axbycz"
+mixString("Hi", "There") → "HTihere"
+mixString("xxxx", "There") → "xTxhxexre"
--- Unit_3/iteration through strings/src/Main.java
+++ Unit_3/iteration through strings/src/Main.java
@@ -1,26 +1,54 @@
 
 public class Main {
     public static void main(String[] args) {
-       //System.out.println(repeatSeparator("this", "X",1));
-         System.out.println(prefixAgain("abXYabc", 1));
+        System.out.println(repeatSeparator("this", "X", 1));
+        System.out.println(prefixAgain("abXYabc", 1));
+        System.out.println(catDog("1cat1cadodog"));
+        System.out.println(endOther("Hiabc", "abc"));
     }
-public static boolean prefixAgain(String prefix, int n) {
-    String prefix2 = prefix.substring(0, n);
-    for (int i = n; i < prefix.length() - n + 1; i++) {
-        if (prefix.substring(i, i + n).equals(prefix2)) {
-            return true;
+
+    public static boolean endOther(String a, String b) {
+        String a2 = a.toLowerCase();
+        String b2 = b.toLowerCase();
+        if (a2.length() > b2.length()) {
+            return a2.substring(a2.length() - b2.length()).equals(b2);
+        } else {
+            return b2.substring(b2.length() - a2.length()).equals(a2);
         }
     }
-    return false;
-}
-public static String repeatSeparator(String word, String sep, int count) {
-    String result = "";
-    for (int i = 0; i < count; i++) {
-        result += word;
-        if (i < count - 1) {
-            result += sep;
+
+    public static boolean catDog(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("cat")) {
+                catCount++;
+            }
+            if (str.substring(i, i + 3).equals("dog")) {
+                dogCount++;
+            }
         }
+        return catCount == dogCount;
     }
-    return result;
-}
+
+    public static boolean prefixAgain(String prefix, int n) {
+        String prefix2 = prefix.substring(0, n);
+        for (int i = n; i < prefix.length() - n + 1; i++) {
+            if (prefix.substring(i, i + n).equals(prefix2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String repeatSeparator(String word, String sep, int count) {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result += word;
+            if (i < count - 1) {
+                result += sep;
+            }
+        }
+        return result;
+    }
 }
