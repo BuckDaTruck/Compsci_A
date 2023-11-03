482edd8df227c80f19f422ba7281562dcf6dcff8
BuckDaTruck <buckleywiley@gmail.com>
2023-11-03 10:12:55 -0400

    commit

--- Unit_4/Unit_4_Project/src/Unit4Project.java
+++ Unit_4/Unit_4_Project/src/Unit4Project.java
@@ -12,12 +12,33 @@ import java.util.Scanner;
 
 public class Unit4Project {
     public static void main(String[] args) {
+        System.out.println("Starting Weather Data Analysis");
+        wait(100);
+        System.out.println("Starting Weather Data Analysis.");
+        wait(100);
+        System.out.println("Starting Weather Data Analysis..");
+        wait(100);
+        System.out.println("Starting Weather Data Analysis...");
+        wait(500);
+        System.out.println("Loading Data");
+        wait(100);
+        System.out.println("Loading Data.");
+        wait(100);
+        System.out.println("Loading Data..");
+        wait(100);
+        System.out.println("Loading Data...");
+        wait(100);
+        System.out.println(
+                "╔╗╔╗╔╗╔╗────────╔══╗────╔╗─────╔╗\n║║║║║║║║────────╚╣╠╝────║║────╔╝╚╗\n║║║║║╠╣║╔══╦╗─╔╗─║║╔═╗╔═╝╠╗╔╦═╩╗╔╬═╦╦══╦══╗\n║╚╝╚╝╠╣║║║═╣║─║║─║║║╔╗╣╔╗║║║║══╣║║╔╬╣║═╣══╣\n╚╗╔╗╔╣║╚╣║═╣╚═╝║╔╣╠╣║║║╚╝║╚╝╠══║╚╣║║║║═╬══║\n─╚╝╚╝╚╩═╩══╩═╗╔╝╚══╩╝╚╩══╩══╩══╩═╩╝╚╩══╩══╝\n───────────╔═║\n───────────╚══╝\n");
+        wait(1000);
         int points = 3;
         int days = 365;
         String[] data = readData();
         int[][] hla = new int[days][points];
         int[][] monthStartEnd = new int[12][2];
         int[][] hlaMonth = new int[12][3];
+        // I am aware this is a 2d array and no chatgpt did not generate that i just
+        // wanted to make it easier to code.
         int[] hlaYear = new int[days];
         parseMonth(data, monthStartEnd, days);
         parseHLA(data, hla, days);
@@ -25,10 +46,12 @@ public class Unit4Project {
         for (int i = 0; i < 12; i++) {
             System.out.println("Month " + (i + 1) + " starts at position " + monthStartEnd[i][0]
                     + " and ends at position " + monthStartEnd[i][1]);
+            wait(20);
         }
         for (int i = 0; i < days; i++) {
             System.out.println(
                     "day " + (i + 1) + " high is " + hla[i][0] + " low is " + hla[i][1] + " average is " + hla[i][2]);
+            wait(5);
         }
         boolean end = false;
         while (!end) {
@@ -46,8 +69,10 @@ public class Unit4Project {
                 for (int i = 0; i < 12; i++) {
                     System.out.println("Month " + (i + 1) + " high is " + hlaMonth[i][0] + " low is " + hlaMonth[i][1]
                             + " average is " + hlaMonth[i][2]);
-                    hlaYear(hla, days, hlaYear);
+                    wait(100);
                 }
+                hlaYear(hla, days, hlaYear);
+                  wait(1000);
             }
             if (choice == 5) {
                 end = true;
@@ -57,22 +82,74 @@ public class Unit4Project {
     }
 
     public static void largeTempChange(String[] data, int[][] hla, int days) {
+        int dayNumber = -1;
+        Scanner Datescanner1 = new Scanner(System.in);
+        System.out.print("Enter the first date using (mm/dd/yyyy): ");
+        String date = Datescanner1.nextLine();
+
+        for (int i = 0; i < days; i++) {
+            String[] parts = data[i].split("-");
+            if (date.equals(parts[0])) {
+                dayNumber = i;
+            }
+        }
+        if (dayNumber == -1) {
+            System.out.println(
+                    "Invalid Date not found in the dataset. Make sure the date is in the correct format.(mm/dd/yyyy)");
+            wait(1000);
+            return;
+        }
+        int dayNumber2 = -1;
+        Scanner Datescanner2 = new Scanner(System.in);
+        System.out.print("Enter the first date using (mm/dd/yyyy): ");
+        String date2 = Datescanner2.nextLine();
+
+        for (int i = 0; i < days; i++) {
+            String[] parts = data[i].split("-");
+            if (date2.equals(parts[0])) {
+                dayNumber2 = i;
+            }
+        }
+        if (dayNumber2 == -1) {
+            System.out.println(
+                    "Invalid Date not found in the dataset. Make sure the date is in the correct format.(mm/dd/yyyy)");
+            wait(1000);
+            return;
+        }
+
         int maxTemperatureChange = 0;
         String dateWithMaxChange = "";
-        for (int i = 1; i < days - 1; i++) {
+        int span = dayNumber2 - dayNumber;
+        for (int i = dayNumber; i <= dayNumber2; i++) {
             int temperatureChange = Math.abs(hla[i][0] - hla[i][1]);
             if (temperatureChange > maxTemperatureChange) {
                 maxTemperatureChange = temperatureChange;
                 dateWithMaxChange = data[i].split("-")[0];
             }
         }
-        System.out.println(dateWithMaxChange + " had the largest temperature change of " + maxTemperatureChange);
+        System.out.println(dateWithMaxChange + " had the largest temperature change of " + maxTemperatureChange
+                + " between the dates" + date + " and " + date2 + " spanning " + span + " days.");
+    wait(1000);
+        /*
+         * int maxTemperatureChange = 0;
+         * String dateWithMaxChange = "";
+         * for (int i = 1; i < days - 1; i++) {
+         * int temperatureChange = Math.abs(hla[i][0] - hla[i][1]);
+         * if (temperatureChange > maxTemperatureChange) {
+         * maxTemperatureChange = temperatureChange;
+         * dateWithMaxChange = data[i].split("-")[0];
+         * }
+         * }
+         * System.out.println(dateWithMaxChange +
+         * " had the largest temperature change of " + maxTemperatureChange);
+         */
     }
 
     public static int choice() {
         Scanner myObj = new Scanner(System.in);
         System.out.println("1. Show the highest and lowest temperature for a particular date.");
-        System.out.println("2. Provide the overall highest, lowest temperature and average temperature for a particular month.");
+        System.out.println(
+                "2. Provide the overall highest, lowest temperature and average temperature for a particular month.");
         System.out.println("3. Find the date of the most drastic temperature change between a range of 2 dates");
         System.out.println("4. Print out yearly statistics");
         System.out.println("5. Exit the program");
@@ -96,13 +173,14 @@ public class Unit4Project {
         Scanner entermnthScanner = new Scanner(System.in);
         System.out.print("Enter the month number: ");
         int month = entermnthScanner.nextInt();
-        if (month < 12 || month > 0) {
+        if (month < 13 && month > 0) {
             System.out.print("High " + hlaMonth[month - 1][0]);
             System.out.print(" Low " + hlaMonth[month - 1][1]);
             System.out.println(" Average " + hlaMonth[month - 1][2]);
         } else {
             System.out.println("Invalid month number.");
         }
+          wait(1000);
     }
 
     public static void lineFromDate(String[] data, int[][] hla, int days) {
@@ -118,7 +196,8 @@ public class Unit4Project {
             }
         }
         if (dayNumber == -1) {
-            System.out.println("Date not found.");
+            System.out.println("Date not found in the dataset.");
+              wait(1000);
             return;
         }
         System.out.println(
@@ -138,6 +217,7 @@ public class Unit4Project {
         } else {
             System.out.println("No data available for the day after.");
         }
+          wait(1000);
     }
 
     public static void hlaYear(int[][] hla, int days, int[] hlaYear) {
@@ -158,9 +238,8 @@ public class Unit4Project {
             temp += hla[i][2];
         }
         hlaYear[2] = temp / days;
-        System.out.print("High " + hlaYear[0]);
-        System.out.print(" Low " + hlaYear[1]);
-        System.out.println(" Average " + hlaYear[2]);
+        System.out.print("This years stats: High " + hlaYear[0] + " Low " + hlaYear[1] + " Average " + hlaYear[2]);
+          wait(1000);
     }
 
     public static void hlaMonthStartEnd(int[][] hla, int[][] monthStartEnd, int days, int points, int[][] hlaMonth) {
@@ -188,7 +267,6 @@ public class Unit4Project {
             hla[i][0] = Integer.parseInt(temps[2]);
             hla[i][1] = Integer.parseInt(temps[1]);
             hla[i][2] = (hla[i][0] + hla[i][1]) / 2;
-
         }
     }
 
